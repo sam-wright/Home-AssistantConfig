641874035b2e3e7b78f021d5275a99c3a1c6b503
Sam Wright <theirrationaldevotion@gmail.com>
2017-08-07 23:51:44 -0600

    simple script to upgrade version and restart service

---
+++ upgrade_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Become the homeassistant user
+#sudo su -s /bin/bash homeassistant;
+
+# Setup Virtual-env
+#source /srv/homeassistant/bin/activate;
+
+
+# Upgrade
+sudo -H -u homeassistant /bin/bash -c 'source /srv/homeassistant/bin/activate && pip3 install --upgrade homeassistant';
+
+# Restart service
+sudo systemctl restart home-assistant@homeassistant.service 
+
+# Done
+exit
