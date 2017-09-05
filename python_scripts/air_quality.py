439e0b1f7cbda9cd077c01231db44a155bbb36e2
Sam Wright <theirrationaldevotion@gmail.com>
2017-09-05 00:02:03 -0600

    adding a python script to conver AQI levels to the more familiar 'green', 'yellow', etc... with advisories

---
+++ python_scripts/air_quality.py
@@ -0,0 +1,36 @@
+aqi_state = hass.states.get('sensor.waqi_salt_lake_city_utah').state
+logger.warning(aqi_state)
+aqi_state = int(aqi_state)
+
+if aqi_state < 50:
+    color = 'green'
+    health_implications = 'Air quality is considered satisfactory, and air pollution poses little or no risk'
+    cautionary_statement = 'None'
+elif aqi_state < 100:
+    color = 'yellow'
+    health_implications = 'Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.'
+    cautionary_statement = 'Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.'
+elif aqi_state < 150:
+    color = 'orange'
+    health_implications = 'Members of sensitive groups may experience health effects. The general public is not likely to be affected.'
+    cautionary_statement = 'Active children and adults, and people with respiratory disease, such as asthma, should limit prolonged outdoor exertion.'
+elif aqi_state < 200:
+    color = 'red'
+    health_implications = 'Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects'
+    cautionary_statement = 'Active children and adults, and people with respiratory disease, such as asthma, should avoid prolonged outdoor exertion; everyone else, especially children, should limit prolonged outdoor exertion'
+elif aqi_state < 300:
+    color = 'purple'
+    health_implications = 'Health warnings of emergency conditions. The entire population is more likely to be affected.'
+    cautionary_statement = 'Active children and adults, and people with respiratory disease, such as asthma, should avoid all outdoor exertion; everyone else, especially children, should limit outdoor exertion.'
+elif aqi_state > 300:
+    color = 'black'
+    health_implications = 'Health alert: everyone may experience more serious health effects'
+    cautionary_statement = 'Everyone should avoid all outdoor exertion'
+
+
+hass.states.set('sensor.aqi', color, {
+    'friendly_name': 'AQI',
+    'health_implications': health_implications,
+    'cautionary_statement': cautionary_statement
+    })
+
